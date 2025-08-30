315bd8677c89636686e1fd97150759c95f132361
Christoph Noe <noe@neonnoe.com>
2025-08-30 20:58:37 +0200

    script added at startup to log versions

---
+++ backend/src/scripts/printVersions.js
@@ -0,0 +1,3 @@
+console.log("Medusa:", require("@medusajs/medusa/package.json").version);
+console.log("Framework:", require("@medusajs/framework/package.json").version);
+console.log("CLI:", require("@medusajs/cli/package.json").version);
